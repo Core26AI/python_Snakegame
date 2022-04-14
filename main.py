46c65b5959aba7905bf915d24c48d6d4bd24dd4c
Core26AI <26renzi@gmail.com>
2022-04-14 22:02:08 +0800

    changed some resources and added game logic

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
-
+import random
 import pygame
 from pygame.locals import *
 
-SIZE = 64
+SIZE = 40
 
 class Apple:
     def __init__(self, parent_screen):
-        self.image = pygame.image.load("resources/apple.png").convert()
+        self.image = pygame.image.load("resources/Ap.jpg").convert()
         self.parent_screen = parent_screen
         self.x = SIZE*3
         self.y = SIZE*3
@@ -17,15 +17,25 @@ class Apple:
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = random.randint(0,24)*SIZE
+        self.y = random.randint(0,19)*SIZE
 
 class Snake:
     def __init__(self, surface, length):
-        self.length = length
         self.parent_screen = surface
-        self.block = pygame.image.load("resources/Block.png").convert()
+        self.block = pygame.image.load("resources/Block2.jpg").convert()
+        self.direction = 'down'
+
+        self.length = length
         self.x = [SIZE]*length
         self.y = [SIZE]*length
-        self.direction = 'down'
+
+    def increase_length(self):
+        self.length+=1
+        self.x.append(-1)
+        self.y.append(-1)
+
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
@@ -67,14 +77,32 @@ class Game:
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((1000, 800))
-        self.snake = Snake(self.surface, 4)
+        self.snake = Snake(self.surface, 1)
         self. snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 <= x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+
+        return False
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0],self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"Score: {self.snake.length}", True, (200,200,200))
+        self.surface.blit(score, (800,10))
 
     def run(self):
         running = True
